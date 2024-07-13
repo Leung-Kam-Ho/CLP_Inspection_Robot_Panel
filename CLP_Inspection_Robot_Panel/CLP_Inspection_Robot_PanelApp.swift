2cdf069176dbcfa8a3d3ea6dc044ea9323ca389e
Kam Ho <71874669+Leung-Kam-Ho@users.noreply.github.com>
2024-07-14 02:19:15 +0800

    Initial Commit

---
+++ CLP_Inspection_Robot_Panel/CLP_Inspection_Robot_PanelApp.swift
@@ -0,0 +1,17 @@
+//
+//  CLP_Inspection_Robot_PanelApp.swift
+//  CLP_Inspection_Robot_Panel
+//
+//  Created by Kam Ho Leung on 14/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct CLP_Inspection_Robot_PanelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CLP_Inspection_Robot_Panel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CLP_Inspection_Robot_Panel
+//
+//  Created by Kam Ho Leung on 14/7/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
